263e79afc188130870c8e8deb08289775ddfe903
남경호 <veluxer62@gmail.com>
2020-04-25 16:39:39 +0900

    Create OrderRepository interface

---
+++ src/main/java/net/class101/server1/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package net.class101.server1.repository;
+
+import net.class101.server1.domain.Order;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OrderRepository {
+    List<Order> findByIdIn(List<UUID> orderIds);
+    void save(Order order);
+}
